b0b55eb0eece982c605b06238ef5e6eda152641e
Daming Yang <topologyYDM@gmail.com>
2024-03-09 14:16:36 +0100

    add function to overplot covar

--- src/corner/__init__.py
+++ src/corner/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ["corner", "hist2d", "quantile", "overplot_lines", "overplot_points", "overplot_hist"]
+__all__ = ["corner", "hist2d", "quantile", "overplot_lines", "overplot_points", "overplot_hist", "overplot_covar"]
 
-from corner.core import hist2d, overplot_lines, overplot_points, quantile, overplot_hist
+from corner.core import hist2d, overplot_lines, overplot_points, quantile, overplot_hist, overplot_covar
 from corner.corner import corner
